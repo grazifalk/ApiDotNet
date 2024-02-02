5d014db1fefeda82b6300848560c402f03e33ee3
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2024-02-02 17:32:26 -0300

    cobertura de testes

---
+++ cobertura_teste.bat
@@ -0,0 +1,6 @@
+﻿mkdir .coverage
+
+dotnet test ApiDotNet.UnitTest/ApiDotNet.UnitTest.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput="../.coverage/Data.opencover.xml" 
+reportgenerator "-reports:.coverage/Data.opencover.xml" "-targetdir:.coverage/.coverage-report" -reporttypes:Html
+
+PAUSE
